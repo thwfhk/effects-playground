b95ffc743813a975ca7a834ff91239f45e5422ba
Wenhao Tang <tangwh1999@gmail.com>
2024-11-19 11:47:30 +0000

    the other order

--- src/ModularCarrier.hs
+++ src/ModularCarrier.hs
@@ -1,26 +1,43 @@
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE DeriveFunctor #-}
+module ModularCarrier where
 
 -- Implementation of Modular Carriers
 -- From the paper: N. Wu and T. Schrijvers, “Efficient Algebraic Effect Handlers,” p. 22.
 
-module ModularCarrier where
+import FreeMonad (Free(..), fold, type (+)(..), Void, (#))
+import Control.Monad
 
--- class ModularCarrier c where
-  -- fwdMC :: Monad m => m (c m) -> c m
+class MCarrier c where
+  fwd :: Monad m => m (c m) -> c m
 
--- fwdsig :: (Functor sig, ModularCarrier c) => sig (c (Free sig)) -> c (Free sig)
--- fwdsig op = fwdMC (Op (fmap Return op))
+forward :: (Functor sig, MCarrier c) => sig (c (Free sig)) -> c (Free sig)
+forward op = fwd (Op (fmap Return op))
 
--- -- Carrier for State s
--- newtype StateH s a m = StateH {runStateH :: s -> m a}
--- instance ModularCarrier (StateH s a) where
---   fwdMC :: (Monad m) => m (StateH s a m) -> StateH s a m
---   fwdMC mf = StateH (\s -> mf >>= \f -> runStateH f s)
+newtype FreeEM a m = FreeEM { unFreeEM :: m a }
+instance MCarrier (FreeEM a) where
+  fwd = FreeEM . join . fmap unFreeEM
 
--- handleSH :: Monad m => Free (State s) a -> StateH s a m
--- handleSH = fold genSH algSH
---   where
---     genSH :: Monad m => a -> StateH s a m
---     genSH x = StateH (\s -> return x)
---     algSH :: Monad m => State s (StateH s a m) -> StateH s a m
---     algSH (Get k) = StateH (\s -> runStateH (k s) s)
---     algSH (Put s' k) = StateH (\s -> runStateH (k ()) s')
+data MHandler sig c a b = MHandler {
+    gen :: forall m . Monad m => a -> c m,
+    alg :: forall m . Monad m => sig (c m) -> c m,
+    run :: forall m . Monad m => c m -> m b
+}
+
+handle :: (Functor f, Functor sig, MCarrier c)
+       => MHandler sig c a b -> Free (sig + f) a -> Free f b
+handle h = run h . fold (gen h) (alg h # forward)
+
+-- Evil
+
+newtype Evil a = Evil a deriving (Show, Functor)
+
+newtype EvilCarrier a m = EC { unEC :: m (() -> m a) }
+
+hEvil :: MHandler Evil (EvilCarrier a) a a
+hEvil = MHandler {
+  gen = \x -> EC . return $ \() -> return x,
+  alg = \(Evil k) -> undefined,
+  run = undefined
+}
--- src/OrpheasState.hs
+++ src/OrpheasState.hs
@@ -3,6 +3,11 @@
 
 module OrpheasState where
 
+-- An interesting implementation of state handler with one get and one
+-- put handlers. Different from the approach in Handle with Care, this
+-- encoding does not need lift because of the usage of state-passing
+-- style.
+
 import FreeMonad (Free(..), fold, fwdPP, type (+)(..), Void, run, (#))
 
 newtype Get s k = Get (s -> k)
@@ -33,6 +38,26 @@ hState = hPut . hGet
         algPut :: Functor f => Put s (s -> Free f a) -> s -> Free f a
         algPut (Put s k) = \_ -> k () s
 
+hState' :: Functor f => Free (Put s + Get s + f) a -> (s -> Free f a)
+hState' = hGet . hPut
+  where
+    hGet :: Functor f => Free (Get s + f) (s -> Free f a) -> (s -> Free f a)
+    hGet = fold genGet (algGet # fwdPP)
+      where
+        genGet :: Functor f => (s -> Free f a) -> s -> Free f a
+        genGet x = x
+        algGet :: Functor f => Get s (s -> Free f a) -> s -> Free f a
+        algGet (Get k) = \s -> k s s
+    hPut :: Functor f => Free (Put s + Get s + f) a -> Free (Get s + f) (s -> Free f a)
+    hPut = fold genPut (algPut # Op)
+      where
+        genPut :: Functor f => a -> Free (Get s + f) (s -> Free f a)
+        genPut x = return (\s -> return x)
+        algPut :: Functor f => Put s (Free (Get s + f) (s -> Free f a))
+                            -> Free (Get s + f) (s -> Free f a)
+        algPut (Put s k) = return (\_ -> hGet (k ()) s)
+
+
 get :: Functor f => Free (Get s + Put s + f) s
 get = Op (Inl $ Get return)
 
@@ -41,6 +66,9 @@ put s = Op (Inr . Inl $ Put s return)
 
 evalState :: s -> Free (Get s + Put s + Void) a -> a
 evalState s x = run (hState x s)
+
+evalState' :: s -> Free (Get s + Put s + Void) a -> a
+evalState' s x = run (hState x s)
 
 prog1 :: Free (Get Int + Put Int + Void) Int
 prog1 = do
@@ -51,6 +79,9 @@ prog1 = do
 -- >>> evalState 0 prog1
 -- 1
 
+-- >>> evalState' 0 prog1
+-- 1
+
 prog2 :: Free (Get Int + Put Int + Void) Int
 prog2 = do
   put 42
@@ -59,4 +90,7 @@ prog2 = do
   get
 
 -- >>> evalState 0 prog2
+-- 84
+
+-- >>> evalState' 0 prog2
 -- 84
